1dcc36b0947cd676ecddeab18b4803ee1ccad13d
rishi-m100 <42911671+rishi-m100@users.noreply.github.com>
2023-12-28 04:41:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+var left=document.getElementsByClassName("left");
+var right=document.getElementsByClassName("right");
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -42,93 +43,110 @@ function autoSlides() {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " alive";
-  setTimeout(autoSlides, 5000); // Change image every 2 seconds
-}
-
-
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.ani');
-    const navLinks = document.querySelectorAll('.menu');
-
-
-
-
-    burger.addEventListener('click', () => {
-        //toggle nav
-
-        nav.classList.toggle('nav-active');
-
-    
-       
-
-        document.getElementById("menu-toggle").addEventListener("mouseover", function(){
-                if (nav.classList.contains('nav-active')) {
-                    var x = document.getElementById("ani-toggle");
-                    x.style.position = "relative";
-                }
-                else {
-                    var x = document.getElementById("ani-toggle");
-                    x.style.removeProperty('position');
-
-                }
-
-            });
-
-
-        document.getElementById("menu-toggle2").addEventListener("mouseover", function () {
-            if (nav.classList.contains('nav-active')) {
-                var x = document.getElementById("ani-toggle2");
-                x.style.position = "relative";
-            }
-            else {
-                var x = document.getElementById("ani-toggle2");
-                x.style.removeProperty('position');
-
-            }
-
-        });
-
-        document.getElementById("menu-toggle3").addEventListener("mouseover", function () {
-            if (nav.classList.contains('nav-active')) {
-                var x = document.getElementById("ani-toggle3");
-                x.style.position = "relative";
-            }
-            else {
-                var x = document.getElementById("ani-toggle3");
-                x.style.removeProperty('position');
-
-            }
-
-        });
-
-       
-       
-
-
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            }
-            else {
-                link.style.animation = link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('cool');
-
-       
-        
-        
-       
-
-    });
+  timer=setTimeout(autoSlides, 4000);
+  timer; // Change image every 2 seconds
+  left[0].addEventListener("click", function() {
+  clearInterval(timer);
+});
+right[0].addEventListener("click", function() {
+  clearInterval(timer);
+});
 
 }
 
-navSlide();
+
+
+//Number Animation
+
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+const obj1 = document.getElementById("value1");
+const obj2 = document.getElementById("value2");
+/*const obj3 = document.getElementById("value3");
+const obj4 = document.getElementById("value4");
+const obj5 = document.getElementById("value5");
+const obj6 = document.getElementById("value6");
+const obj7 = document.getElementById("value7");
+*/
 
 
 
+function isInViewport(element) {
+    var element = document.getElementById('value1');
+
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+
+}
+    var element = document.getElementById('value1');
+    var condition = isInViewport();
+    var count = 0;
+
+function animateNumbers() {
+
+if (isInViewport()==true) {
+  if (count== 0) {
+animateValue(obj1, 0, 4, 2000);
+animateValue(obj2, 0, 84, 3000);
+//animateValue(obj3, 0, 64, 2500);
+
+  }
+count=1;
+}
+}
+document.getElementById('all').addEventListener("mousemove", animateNumbers);
+document.getElementById('all').addEventListener("wheel", animateNumbers);
+
+
+
+//
+
+
+
+function isInViewport2(element2) {
+    var element2 = document.getElementById('value4');
+
+    const rect = element2.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+
+}
+    var element2 = document.getElementById('value4');
+    var condition2 = isInViewport();
+    var count2 = 0;
+
+function animateNumbers2() {
+
+if (isInViewport2()==true) {
+  if (count2== 0) {
+animateValue(obj4, 0, 6, 2000);
+animateValue(obj5, 0, 70, 3000);
+animateValue(obj6, 0, 3, 2000);
+animateValue(obj7, 0, 14, 2500);
+  }
+count2=1;
+}
+}
+document.getElementById('all').addEventListener("mousemove", animateNumbers2);
+document.getElementById('all').addEventListener("wheel", animateNumbers2);
 
